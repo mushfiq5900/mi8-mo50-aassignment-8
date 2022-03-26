57a4a364b3a864cd94b69dc82b3d88008a843640
Mushfiq <mushfiquli@gmail.com>
2022-03-26 13:52:57 +0600

    ring card design touched

--- src/components/ringview/RingView.js
+++ src/components/ringview/RingView.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './RingView.css'
 
 const RingView = (props) => {
     const { name, img, brand, price } = props.ring
     return (
-        <div className="ring-list">
-            <img src={img} alt={name} />
-            <div className="ring-details">
-                <h3>{name}</h3>
-                <h4>Price:{price}</h4>
-                <h5>Brand Name:{brand}</h5>
+        <div className='container-div'>
+            <div className="ring-card">
+                <img src={img} alt={name} />
+                <div className="ring-details">
+                    <h3 className='ring-name'>{name}</h3>
+                    <h4>Price: {price}$</h4>
+                    <h5>Brand Name: {brand}</h5>
+                </div>
+                <button onClick={() => props.clicked(props.ring)} className='selection-btn'>Add to Selection</button>
             </div>
-            <button onClick={() => props.clicked(props.ring)}>Add to Cart</button>
         </div>
     );
 };
