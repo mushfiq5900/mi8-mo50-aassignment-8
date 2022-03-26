7f761d2ee64225d9c0bb86bed0f0747f33675a2d
Mushfiq <mushfiquli@gmail.com>
2022-03-26 19:14:34 +0600

    Qus And Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from '././components/site_title/Title';
 import Allrings from './components/allrings/Allrings';
+import QusAns from './components/qus_ans/QusAns';
 
 function App() {
   return (
     <div>
       <Title></Title>
       <Allrings></Allrings>
+      <QusAns></QusAns>
     </div>
   );
 }
---
+++ src/components/qus_ans/QusAns.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './QusAns.css'
+
+const QusAns = () => {
+    return (
+        <div className='qus-ans-div'>
+            <h1>How react works?</h1>
+            <p>React is a library of javascript. In react JSX is used to write html codes. There are also some intersting functions in it. React is use for mainly user interface design. Both mobile and web pages are beign build with react.</p>
+            <h1>How useState works?</h1>
+            <p>useState is known as hook. There are two parts of useState. One is a variable, and a set variable. Another one is the current state. If I pass something initially it will return a variable with the current state. And also returns an another function to update vlaue.</p>
+        </div>
+    );
+};
+
+export default QusAns;
